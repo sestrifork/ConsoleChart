5e163ddb63b414ecc7287ae26c46b93608dcbe42
sestrifork <ses@trifork.com>
2023-03-26 17:08:14 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+ConsoleCanvas consoleCanvas = new ConsoleCanvas();
+
+consoleCanvas.DrawMaxFrame();
+for (int i=1; i<consoleCanvas.Width; i=i+4) {
+    int value = i * 42 % consoleCanvas.Height;
+    ConsoleColor color = ConsoleColor.Green;
+    if (value < consoleCanvas.Height/2) {
+        color = ConsoleColor.Red;
+    }
+    consoleCanvas.Rect(new Point(i,consoleCanvas.Height-value-1),1,value, color);
+}
+Console.ReadKey();
